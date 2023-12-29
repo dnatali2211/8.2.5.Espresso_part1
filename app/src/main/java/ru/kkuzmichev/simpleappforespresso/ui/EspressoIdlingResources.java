d9058666c0a29f866be24478655438db898d7b1e
Nataliia Bulatovskaia <dnatali2211@gmail.com>
2023-12-29 12:00:43 +0400

    Added IdlingTest class

---
+++ app/src/main/java/ru/kkuzmichev/simpleappforespresso/ui/EspressoIdlingResources.java
@@ -0,0 +1,12 @@
+package ru.kkuzmichev.simpleappforespresso.ui;
+import androidx.test.espresso.idling.CountingIdlingResource;
+public class EspressoIdlingResources {
+private static final String RESOURCE = "Global";
+public static CountingIdlingResource idlingResource = new CountingIdlingResource(RESOURCE);
+public static void increment() { idlingResource.increment(); }
+    public static void decrement() {
+    if (!idlingResource.isIdleNow()) {
+        idlingResource.decrement();
+    }
+    }
+}
